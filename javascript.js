349abd3b4bae6b92541e797f5eef426ec9fec68c
Htoo Aung <kyawaung0924@gmail.com>
2024-04-30 02:51:41 -0600

    Cleaned up fillBoard() for better memory usage and readability.

--- javascript.js
+++ javascript.js
@@ -2,18 +2,17 @@ const drawingBoard = document.getElementById("drawing-container");
 
 function fillBoard(boxes) {
     const dimensions = boxes * boxes;
+    
+    const drawingBoardDim = window.getComputedStyle(drawingBoard);
+    const drawingBoardWidth = drawingBoardDim.width.slice(0, 3);
+    const drawingBoardHeight = drawingBoardDim.height.slice(0, 3);
+    
+    const boxWidth = Number(drawingBoardWidth) / boxes;
+    const boxHeight = Number(drawingBoardHeight) / boxes;
 
     for (let i = 0; i < dimensions; i++) {
         const box = document.createElement("div");
         box.classList.add("box");
-
-        let drawingBoardDim = window.getComputedStyle(drawingBoard);
-
-        const drawingBoardWidth = drawingBoardDim.width.slice(0, 3);
-        const drawingBoardHeight = drawingBoardDim.height.slice(0, 3);
-
-        const boxWidth = Number(drawingBoardWidth) / boxes;
-        const boxHeight = Number(drawingBoardHeight) / boxes;
 
         box.style.width = `${boxWidth}px`;
         box.style.height = `${boxHeight}px`;
