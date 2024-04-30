7ceab8c57ed04461f9aceb2b44d0998bc0567108
Htoo Aung <kyawaung0924@gmail.com>
2024-04-30 03:33:37 -0600

    Added a font to make it presentable.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
 const drawingBoard = document.getElementById("drawing-container");
 
+const applyBtn = document.getElementById("apply-btn");
+const eraseBtn = document.getElementById("erase-btn");
+const multicolorBtn = document.getElementById("multicolor-btn");
+
+
+
 function fillBoard(boxes) {
     const dimensions = boxes * boxes;
-    
+
     const drawingBoardDim = window.getComputedStyle(drawingBoard);
     const drawingBoardWidth = drawingBoardDim.width.slice(0, 3);
     const drawingBoardHeight = drawingBoardDim.height.slice(0, 3);
@@ -18,8 +24,6 @@ function fillBoard(boxes) {
         box.style.height = `${boxHeight}px`;
 
         drawingBoard.appendChild(box);
-
-        console.log(boxWidth)
     }
 }
 
