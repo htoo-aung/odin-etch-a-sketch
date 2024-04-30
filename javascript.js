88e49b39bc85e11779df9a65ae197758fb0ba9df
Htoo Aung <kyawaung0924@gmail.com>
2024-04-30 02:47:30 -0600

    fillBoard() function implemented to fill the drawing board with boxes.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,26 @@
 const drawingBoard = document.getElementById("drawing-container");
 
-function addBox(boxes) {
-    for (let i = 0; i < (boxes*boxes); i++) {
+function fillBoard(boxes) {
+    const dimensions = boxes * boxes;
+
+    for (let i = 0; i < dimensions; i++) {
         const box = document.createElement("div");
         box.classList.add("box");
 
-        const boxWidth = drawingBoard.style.width / boxes;
-        const boxHeight = drawingBoard.style.height / boxes;
+        let drawingBoardDim = window.getComputedStyle(drawingBoard);
 
-        box.style.width = boxWidth + "px";
-        box.style.height = boxHeight + "px";
+        const drawingBoardWidth = drawingBoardDim.width.slice(0, 3);
+        const drawingBoardHeight = drawingBoardDim.height.slice(0, 3);
+
+        const boxWidth = Number(drawingBoardWidth) / boxes;
+        const boxHeight = Number(drawingBoardHeight) / boxes;
+
+        box.style.width = `${boxWidth}px`;
+        box.style.height = `${boxHeight}px`;
 
         drawingBoard.appendChild(box);
+
+        console.log(boxWidth)
     }
 }
+
